5d9d5a1ac9a745e5e6da45d555d1cb433ead49b0
Swapnilpatil03 <swapnilpatil20040@gmail.com>
2025-06-18 23:59:25 +0530

    Initial commit - SplitApp with dashboard and update feature

--- app.js
+++ app.js
@@ -2,22 +2,20 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 
-
-
-
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/', 'index.html'));
-});
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 app.use('/expenses', require('./routes/expenseRoutes'));
 app.use('/settlements', require('./routes/settlementRoutes'));
 app.use('/balances', require('./routes/balanceRoutes'));
 app.use('/people', require('./routes/peopleRoutes'));
+
 
 module.exports = app;
